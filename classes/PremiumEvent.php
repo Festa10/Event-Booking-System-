2895b58734619deec09590a7cc32c866c68a1483
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-04-26 22:58:25 +0200

    Update PremiumEvent
    Refactor PremiumEvent class constructor to include benefit parameter.

--- classes/PremiumEvent.php
+++ classes/PremiumEvent.php
@@ -1,19 +1,17 @@
 <?php
-// 1. Marrim klasen Event nga kolegia
+
+
 require_once __DIR__ . '/Event.php';
 
-// 2. Krijojmë vetëm klasën tënde PremiumEvent
-// Kujdes: Kjo do të funksionojë nëse klasa Event e koleges i pranon këto parametra
+// 2. Trashëgimia e klasës PremiumEvent nga Event
 class PremiumEvent extends Event {
     public $benefit;
 
     public function __construct($id, $title, $date, $price, $benefit) {
-        // Këtu i dërgojmë të dhënat te klasa e koleges
-        // Nëse ajo ka (title, date, location, price), atëherë:
-        // $id yt do të bëhet title i saj
-        // $title yt do të bëhet date i saj... etj.
+        // Thirret konstruktori i klasës prind (Event)
         parent::__construct($id, $title, $date, $price);
         $this->benefit = $benefit;
     }
 }
+
 ?>
