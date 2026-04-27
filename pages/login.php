a21f0b61d8b4349181b015e6fb90d4854b573990
Festa10 <festa.berisha3@student.uni-pr.edu>
2026-04-27 12:47:10 +0200

    Permiresime te vogla ne login.php

--- pages/login.php
+++ pages/login.php
@@ -105,18 +105,20 @@ input{
 </div>
 
 <footer>
- <div class="footer-container">
-     <div>
+
+<div class="footer-container">
+
+    <div>
         <h3>About Us</h3>
         <p>We provide the best event booking experience for concerts, weddings, festivals and more.</p>
     </div>
 
     <div>
         <h3>Quick Links</h3>
-        <p>Home</p>
-        <p>Events</p>
-        <p>Booking</p>
-        <p>About</p>
+        <a href="/project/index.php">Home</a><br>
+        <a href="/project/pages/view_event.php">Events<a/><br>
+        <a href="/project/pages/about.php">About</a>
+       
     </div>
 
     <div>
@@ -124,12 +126,11 @@ input{
         <p>Email: eventbooking@gmail.com</p>
         <p>Phone: 044 552 332</p>
     </div>
- </div>
 
- <div class="footer-bottom">
-    © 2026 Event Booking System | All Rights Reserved
- </div>
+</div>
+
+<p class="copyright">© 2026 Event Booking System | All Rights Reserved</p>
+
 </footer>
-
 </body>
 </html>
