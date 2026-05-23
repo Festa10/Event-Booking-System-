9f30868839c1127e804eae4617f9eb63e90b2e5c
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-05-23 20:14:53 +0200

    Add custom error handler in error_handler.php
    Implement custom error handler to throw exceptions.

--- includes/error_handler.php
+++ includes/error_handler.php
@@ -1,1 +1,14 @@
+<?php
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
 
+set_error_handler("myErrorHandler");
+
+try {
+   
+} catch (Exception $e) {
+ 
+    echo "Gabim: " . $e->getMessage();
+}
+?>
