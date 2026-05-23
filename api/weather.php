e95557ceb46bcece4aeba90c4b21ebd0cb99d94c
Jetaa <jeta.podrimcaku@student.uni-pr.edu>
2026-05-23 20:11:34 +0200

    weather API
    Added a function to fetch weather data for a given city using the OpenWeatherMap API.

--- api/weather.php
+++ api/weather.php
@@ -1,1 +1,37 @@
+<?php
 
+
+function getEventWeather($city) {
+    $apiKey = "8797f1f51084803099951336c539207e"; 
+    $url = "https://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . $apiKey . "&units=metric";
+
+    try {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new Exception("Gabim gjatë lidhjes me API: " . curl_error($ch));
+        }
+
+        $data = json_decode($response, true);
+        curl_close($ch);
+
+        if ($data['cod'] != 200) {
+            throw new Exception("Qyteti nuk u gjet ose API Key është i pasaktë.");
+        }
+
+        return [
+            'temp' => $data['main']['temp'],
+            'desc' => $data['weather'][0]['description'],
+            'icon' => $data['weather'][0]['icon']
+        ];
+
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        return null;
+    }
+}
